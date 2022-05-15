9d4983de07a3c5871eceeaded26b7b5d7955ed40
Mubelotix <mubelotix@gmail.com>
2022-05-15 14:36:33 +0200

    Update src/settings.rs
    Co-authored-by: DimitriTimoz <45998396+DimitriTimoz@users.noreply.github.com>

--- src/settings.rs
+++ src/settings.rs
@@ -287,7 +287,7 @@ impl Component for SettingsPage {
                             <div class="setting">
                                 <h4>{t("Adresse mail")}</h4>
                                 <p>{format!("{} {email}.{verified_msg}", t("Votre adresse actuelle est"))}</p>
-                                <div class="primary-button" onclick={move |_| app_link3.send_message(AppMsg::SetPage(Page::ChangeEmail))}>{t("Changer / Vérifier")}</div>
+                                <div class="primary-button" onclick={move |_| app_link3.send_message(AppMsg::SetPage(Page::ChangeEmail))}>{t("Modifier")}</div>
                             </div>
                         </div>
                     </section>
