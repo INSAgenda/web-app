5ce7e09cee083718ef084467acc3b2c07af50e5a
Mubelotix <mubelotix@gmail.com>
2022-05-18 17:43:10 +0200

    Add filter on press

--- src/event.rs
+++ src/event.rs
@@ -141,9 +141,54 @@ impl Component for EventComp {
         let start = Paris.timestamp(ctx.props().event.start_unixtime as i64, 0);
         let end = Paris.timestamp(ctx.props().event.end_unixtime as i64, 0);
         let class = String::new() + if self.popup_displayed {""} else {"hide"};
-
         let mobile = width() <= 1000;
-        html! {
+
+        let popup = html! {
+            <div class={format!("event-details {}", class)} id={self.popup_id.clone()} style={String::new() + if ctx.props().day_of_week > 2 { "left" } else { "right" } + ": -214px;" + if percent_offset > 50. && !mobile {"transform: translateY(-70%);"}  else {""}}  >
+                if mobile {
+                    <div class="close-arrow" onclick={ Some(ctx.link().callback(|_| EventCompMsg::ShowPopup(false))) } >
+                        <svg width="110" height="28" viewBox="0 0 110 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M55.5 28C55.5 28 19.6743 2 0.5 0H55.5V28Z" fill="var(--day)"/>
+                            <path d="M55 28C55 28 90.8257 2 110 0H55V28Z" fill="var(--day)"/>
+                            <g clip-path="url(#clip0_211_6147)">
+                            <path d="M55 18.0015C55.4045 18.0017 55.8051 17.9605 56.1787 17.8802C56.5524 17.7999 56.8918 17.6822 57.1774 17.5337L76.0974 7.72639C76.6752 7.42608 76.9993 7.0191 76.9982 6.59497C76.9971 6.17084 76.6709 5.7643 76.0915 5.4648C75.5121 5.16529 74.7268 4.99736 73.9084 4.99793C73.0901 4.9985 72.3057 5.16752 71.7278 5.46783L54.9883 14.1436L38.2487 5.46783C37.6708 5.16833 36.887 5.00007 36.0698 5.00007C35.2525 5.00007 34.4688 5.16833 33.8909 5.46783C33.313 5.76734 32.9883 6.17355 32.9883 6.59711C32.9883 7.02067 33.313 7.42689 33.8909 7.72639L52.8108 17.5322C53.0976 17.6817 53.4388 17.8001 53.8146 17.8807C54.1904 17.9612 54.5933 18.0023 55 18.0015Z" fill="var(--text)"/>
+                            </g>
+                        </svg>
+                    </div>
+                }
+                <h3>{ &name }</h3>
+                <div style={format!("background-color: {};", bg_color.clone())} class="divider-bar-option"></div>
+                <div class="event-details-content">
+                    <div class="info-block">
+                        <h4>{t("Horaires")}</h4>
+                        <span>{format!("{} - {}", start.time().format("%Hh%M"), end.time().format("%Hh%M"))}</span>
+                    </div>
+
+                    if let Some(location) = &location { 
+                        <div class="info-block">
+                            <h4>{t("Emplacement")}</h4>
+                            <span>{location}</span>
+                        </div>
+                    }
+
+                    if !ctx.props().event.teachers.is_empty() {
+                        <div class="info-block">
+                            <h4>{t("Enseignant")}</h4>
+                            <span>{ctx.props().event.teachers.join(", ")}</span>
+                        </div>
+                    }
+                </div>
+
+                <div class="info-block">
+                    <h4>{t("Couleur")}</h4>
+                    <input type="color" id="background-color-input" value={bg_color.clone()} onchange={ctx.link().callback(|_| EventCompMsg::SaveColors)}  />
+
+                    <span style={format!("background: {}80;", bg_color)}>{t("Fond")}</span>
+                </div>
+            </div>
+        };
+
+        html! {<>
             <div
                 style={format!("background-color: {}80; border-left: 0.3rem solid {};  position: absolute; top: {}%; height: {}%;", bg_color.clone(), bg_color.clone(), percent_offset, percent_height)}
                 class="event" >
@@ -156,49 +201,9 @@ impl Component for EventComp {
                     </span>
                     if let Some(l) = &location { <span class="location" >{l}</span>}
                 </div>
-                <div class={format!("event-details {}", class)} id={self.popup_id.clone()} style={String::new() + if ctx.props().day_of_week > 2 { "left" } else { "right" } + ": -214px;" + if percent_offset > 50. && !mobile {"transform: translateY(-70%);"}  else {""}}  >
-                        if mobile {
-                            <div class="close-arrow" onclick={ Some(ctx.link().callback(|_| EventCompMsg::ShowPopup(false))) } >
-                                <svg width="110" height="28" viewBox="0 0 110 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M55.5 28C55.5 28 19.6743 2 0.5 0H55.5V28Z" fill="var(--day)"/>
-                                    <path d="M55 28C55 28 90.8257 2 110 0H55V28Z" fill="var(--day)"/>
-                                    <g clip-path="url(#clip0_211_6147)">
-                                        <path d="M55 18.0015C55.4045 18.0017 55.8051 17.9605 56.1787 17.8802C56.5524 17.7999 56.8918 17.6822 57.1774 17.5337L76.0974 7.72639C76.6752 7.42608 76.9993 7.0191 76.9982 6.59497C76.9971 6.17084 76.6709 5.7643 76.0915 5.4648C75.5121 5.16529 74.7268 4.99736 73.9084 4.99793C73.0901 4.9985 72.3057 5.16752 71.7278 5.46783L54.9883 14.1436L38.2487 5.46783C37.6708 5.16833 36.887 5.00007 36.0698 5.00007C35.2525 5.00007 34.4688 5.16833 33.8909 5.46783C33.313 5.76734 32.9883 6.17355 32.9883 6.59711C32.9883 7.02067 33.313 7.42689 33.8909 7.72639L52.8108 17.5322C53.0976 17.6817 53.4388 17.8001 53.8146 17.8807C54.1904 17.9612 54.5933 18.0023 55 18.0015Z" fill="var(--text)"/>
-                                    </g>
-                                </svg>
-                            </div>
-                        }
-                        <h3>{ name }</h3>
-                        <div style={format!("background-color: {};", bg_color.clone())} class="divider-bar-option"></div>                   
-                        <div class="event-details-content">
-                            <div class="info-block">
-                                <h4>{t("Horaires")}</h4>
-                                <span>{format!("{} - {}", start.time().format("%Hh%M"), end.time().format("%Hh%M"))}</span>
-                            </div>
-
-                            if let Some(location) = location { 
-                                <div class="info-block">
-                                    <h4>{t("Emplacement")}</h4>
-                                    <span>{location}</span>
-                                </div>
-                            }
-
-                            if !ctx.props().event.teachers.is_empty() {
-                                <div class="info-block">
-                                    <h4>{t("Enseignant")}</h4>
-                                    <span>{ctx.props().event.teachers.join(", ")}</span>
-                                </div>
-                            }
-                        </div>
-
-                        <div class="info-block">
-                            <h4>{t("Couleur")}</h4>
-                            <input type="color" id="background-color-input" value={bg_color.clone()} onchange={ctx.link().callback(|_| EventCompMsg::SaveColors)}  />
-
-                            <span style={format!("background: {}80;", bg_color)}>{t("Fond")}</span>
-                        </div>
-                    </div>
-                </div>
-        }
+                if !mobile {{popup.clone()}} // Due to the brightness filter when events are pressed on mobile, the popup needs to be outside the event
+            </div>
+            if mobile {{popup}}
+        </>}
     }
 }
