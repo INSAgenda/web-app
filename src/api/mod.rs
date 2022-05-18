ea99b4cf254aaf2740854f82970fb51aff15de67
Mubelotix <mubelotix@gmail.com>
2022-05-18 21:44:35 +0200

    New code gen

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -6,21 +6,10 @@ mod user_info;
 pub use user_info::*;
 mod error;
 pub use error::*;
+mod gen_code;
+pub use gen_code::gen_code;
 
 use crate::prelude::*;
-
-fn gen_code(api_key: u64, counter: u64) -> u64 {
-    let mut key = (api_key + 143 * counter) as u128;
-    for _ in 0..11 {
-        key = key * key + 453;
-        if key <= 0xffff_ffff {
-            key += 0x4242424242424242424242424242;
-        }
-        key &= 0x0000_0000_ffff_ffff_ffff_ffff_0000_0000;
-        key >>= 32;
-    }
-    key as u64
-}
 
 fn get_login_info() -> (u64, u64) {
     let local_storage = window().local_storage().unwrap().unwrap();
