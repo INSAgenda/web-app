1af8452a4405253a12e88070b3ba4bd10e8dff73
DimitriTimoz <45998396+DimitriTimoz@users.noreply.github.com>
2022-08-28 03:17:28 +0200

    fix

--- src/settings.rs
+++ src/settings.rs
@@ -6,8 +6,7 @@ lazy_static::lazy_static!{
         let theme = match local_storage.get_item("setting-theme").unwrap() {
             Some(theme) if theme == "dark" => 0,
             Some(theme) if theme == "light" => 1,
-            Some(theme) => {alert(format!("Invalid theme {theme}")); 0},
-            None => 2,
+            _ => 2,
         };
         let building_naming = match local_storage.get_item("setting-building-naming").unwrap() {
             Some(building_naming) if building_naming == "short" => 0,
