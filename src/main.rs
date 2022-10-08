a4e9f51859170ab17b0f5d28e4203b5ba11616a4
Alix ANNERAUD <alix@anneraud.fr>
2022-10-08 19:12:21 +0200

    Fix transition time for desktop on sat/sun.

--- src/main.rs
+++ src/main.rs
@@ -132,10 +132,13 @@ impl Component for App {
         if now.hour() >= 19 || weekday == Weekday::Sat || weekday == Weekday::Sun {
             let link2 = ctx.link().clone();
             spawn_local(async move {
-                sleep(Duration::from_millis(500)).await;
+                if width() <= 1000
+                {
+                    sleep(Duration::from_millis(500)).await;
+                }
                 link2.send_message(Msg::Next);
                 if weekday == Weekday::Sat {
-                    if (width() <= 1000)
+                    if width() <= 1000
                     {
                         sleep(Duration::from_millis(300)).await;
                     }
