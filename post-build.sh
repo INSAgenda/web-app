f2b3d8d37969c1ce83a2b5717eb2d10a5cb812d1
Mubelotix <mubelotix@gmail.com>
2022-12-01 00:22:02 +0100

    Fix script

--- post-build.sh
+++ post-build.sh
@@ -2,9 +2,10 @@
 
 file=$(find dist -name '*.js')
 
-to_replace="= new BigInt64Array();"
-replacement="= undefined; try { cachedBigInt64Memory0 = new BigInt64Array(); } catch (e) { console.error('BigInt64Array is not supported'); }"
+to_replace="cachedBigInt64Memory0 = new BigInt64Array();"
+replacement="cachedBigInt64Memory0 = undefined; try { cachedBigInt64Memory0 = new BigInt64Array(); } catch (e) { console.error('BigInt64Array is not supported'); }"
 sed -i -e "s/$to_replace/$replacement/g" $file
 
-to_replace="= new BigInt64Array(wasm.memory.buffer);"
+to_replace="cachedBigInt64Memory0 = new BigInt64Array(wasm.memory.buffer);"
+replacement="cachedBigInt64Memory0 = undefined; try { cachedBigInt64Memory0 = new BigInt64Array(wasm.memory.buffer); } catch (e) { console.error('BigInt64Array is not supported'); }"
 sed -i -e "s/$to_replace/$replacement/g" $file
