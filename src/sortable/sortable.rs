cfc3210dfd776ab21b8025f8786ceb99803c8841
Mubelotix <mubelotix@gmail.com>
2022-12-04 01:49:04 +0100

    Calculate positions

--- src/sortable/sortable.rs
+++ src/sortable/sortable.rs
@@ -5,13 +5,21 @@ pub struct SortableProps {
     pub items: Vec<String>,
 }
 
+
+struct Positions {
+    start_y: usize,
+    width: usize,
+    heights: Vec<usize>,
+}
+
 pub struct Sortable {
     id: usize,
-    positions: Option<(usize, Vec<usize>)>,
+    positions: Option<Positions>,
     ordered: Vec<usize>,
 }
 
 pub enum SortableMsg {
+    Reload
 }
 
 impl Component for Sortable {
@@ -28,9 +36,41 @@ impl Component for Sortable {
         }
     }
 
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            SortableMsg::Reload => {
+                true
+            }
+        }
+    }
+
     fn changed(&mut self, ctx: &Context<Self>) -> bool {
         *self = Self::create(ctx);
         true
+    }
+
+    fn rendered(&mut self, ctx: &Context<Self>, _first_render: bool) {
+        if self.positions.is_some() { return }
+        
+        let mut heights = Vec::new();
+        let mut start_y = usize::MAX;
+        let mut width = 0;
+        let document = window().doc();
+        for i in 0..self.ordered.len() {
+            let item = document.get_element_by_id(&format!("sortable-{}-{i}", self.id)).unwrap();
+            let rect = item.get_bounding_client_rect();
+            heights.push(rect.height() as usize);
+            start_y = start_y.min(rect.x() as usize);
+            width = width.max(rect.width() as usize);
+        }
+        
+        self.positions = Some(Positions { start_y, width, heights });
+        
+        let link = ctx.link().clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            sleep(Duration::from_secs(5)).await;
+            link.send_message(SortableMsg::Reload); // TESTING ONLY: REMOVE BEFORE MERGE
+        });
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
@@ -39,9 +79,9 @@ impl Component for Sortable {
             let item = ctx.props().items.get(*i).unwrap();
             let fid = format!("sortable-{}-{}", self.id, i);
             let style = match &self.positions {
-                Some(positions) => {
-                    let style = format!("position: absolute; top: {offset}px;");
-                    offset += positions.1.get(*i).unwrap();
+                Some(Positions { start_y: _, width, heights }) => {
+                    let style = format!("position: absolute; top: calc({offset}px + {i} * .5rem); width: calc({width}px - 1rem);");
+                    offset += heights.get(*i).unwrap();
                     style
                 },
                 None => String::new(),
