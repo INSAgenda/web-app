855c98dcf84a5de069ba1a46ea5925a489010974
Mubelotix <mubelotix@gmail.com>
2023-01-18 18:25:19 +0100

    Add first friend section

--- src/friends/friends.rs
+++ src/friends/friends.rs
@@ -14,6 +14,11 @@ impl Component for FriendsPage {
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
+        let names = vec!["John Doe", "Edouart Foobar", "Exponenthis"];
+        let picture_iter = names.iter().map(|name| format!("https://api.dicebear.com/5.x/micah/svg?seed={name}", name = name.replace(" ", "+")));
+        let alt_iter = names.iter().map(|name| format!("Avatar of {name}"));
+        let name_iter = names.iter();
+
         template_html!("src/friends/friends.html", ...)
     }
 }
