ce5bbed7c5f42302b7658da77475030aadef565b
Mubelotix <mubelotix@gmail.com>
2023-05-22 11:40:08 +0200

    Improve display of errors

--- src/api/error.rs
+++ src/api/error.rs
@@ -37,7 +37,12 @@ impl std::fmt::Display for ApiError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             ApiError::Known(e) => e.fmt(f),
-            ApiError::Unknown(e) => write!(f, "{:?}", e),
+            ApiError::Unknown(e) => {
+                match e.as_string() {
+                    Some(s) => write!(f, "{}", s),
+                    None => write!(f, "{:?}", e),
+                }
+            },
         }
     }
 }
