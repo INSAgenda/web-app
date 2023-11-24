016af61800e44e684fb229d5037e99b03457bfc6
Dimitri <dimitri.timoz@orange.fr>
2023-11-24 17:02:59 +0100

    Add methods to retrieve specific gifts and all days in GiftList

--- src/advent/gifts.rs
+++ src/advent/gifts.rs
@@ -1,3 +1,4 @@
+use std::{num::NonZeroU8, collections::HashSet};
 use crate::prelude::*;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -18,5 +19,13 @@ impl GiftList {
        let mut gifts: Vec<Gift> = serde_json::from_str(json).ok()?;
         gifts.sort_by_key(|gift| gift.day);
         Some(Self { gifts })
+    }
+
+    pub fn get_gift(&self, day: NonZeroU8) -> Option<Gift> {
+        self.gifts.get(day.get() as usize - 1).cloned()
+    }
+
+    pub fn get_all_days(&self) -> HashSet<NonZeroU8> {
+        self.gifts.iter().map(|gift| NonZeroU8::new(gift.day).unwrap()).collect()
     }
 }
