476381b0e66c01d05f56393d3fd8396b488eff3f
DimitriTimoz <dimitri.timoz@orange.fr>
2023-11-25 22:26:05 +0100

    Add CollectedGifts

--- src/advent/gifts.rs
+++ src/advent/gifts.rs
@@ -14,6 +14,11 @@ pub struct GiftList {
     pub gifts: Vec<Gift>,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct CollectedGifts {
+    collected: HashSet<u8>
+}
+
 impl GiftList {
     pub fn from_json(json: &str) -> Option<Self> {
        let mut gifts: Vec<Gift> = serde_json::from_str(json).ok()?;
@@ -27,5 +32,28 @@ impl GiftList {
 
     pub fn get_all_days(&self) -> HashSet<NonZeroU8> {
         self.gifts.iter().map(|gift| NonZeroU8::new(gift.day).unwrap()).collect()
+    }
+}
+
+impl CollectedGifts {
+    pub fn from_json(json: &str) -> Option<Self> {
+        let collected: HashSet<u8> = serde_json::from_str(json).ok()?;
+        Some(Self { collected })
+    }
+
+    pub fn get_all_days(&self) -> HashSet<NonZeroU8> {
+        self.collected.iter().map(|day| NonZeroU8::new(*day).unwrap()).collect()
+    }
+
+    pub fn is_collected(&self, day: NonZeroU8) -> bool {
+        self.collected.contains(&day.get())
+    }
+
+    pub fn collect(&mut self, day: NonZeroU8) {
+        self.collected.insert(day.get());
+    }
+
+    pub fn to_json(&self) -> String {
+        serde_json::to_string(&self.collected).unwrap()
     }
 }
