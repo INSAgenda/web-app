80563da14129e30a8051ae09791823a7ba71e7fd
Mubelotix <mubelotix@gmail.com>
2023-11-26 12:35:29 +0100

    Revert "Merge pull request #188 from INSAgenda/force-click"
    This reverts commit 1a9b7489cbd92cba9c1fab263a21cdc860cd5575, reversing
    changes made to 3567a7f9ac9f9de43083c2b18f8b768ee48e4e9b.

--- src/main.rs
+++ src/main.rs
@@ -33,9 +33,6 @@ mod friends;
 mod comment;
 #[path = "notifications/notifications.rs"]
 mod notifications;
-#[path ="force-click/force_click.rs"]
-mod force_click;
-
 mod util;
 mod slider;
 mod api;
