0c63590feded1b61703e1bf6952e81973a3bb32c
Mubelotix <mubelotix@gmail.com>
2024-03-28 21:00:27 +0100

    Restore juliette fixes

--- post-build.sh
+++ post-build.sh
@@ -1,31 +1,5 @@
 #!/bin/sh
 
-# file=$(find dist -name '*.js')
-
-# Juliette fixes
-# to_replace="cachedBigInt64Memory0 = new BigInt64Array();"
-# replacement="cachedBigInt64Memory0 = undefined; try { cachedBigInt64Memory0 = new BigInt64Array(); } catch (e) { console.error('BigInt64Array is not supported'); }"
-# sed -i -e "s/$to_replace/$replacement/g" $file
-# to_replace="cachedBigInt64Memory0 = new BigInt64Array(wasm.memory.buffer);"
-# replacement="cachedBigInt64Memory0 = undefined; try { cachedBigInt64Memory0 = new BigInt64Array(wasm.memory.buffer); } catch (e) { console.error('BigInt64Array is not supported'); }"
-# sed -i -e "s/$to_replace/$replacement/g" $file
-
-# file="dist/index.html"
-
-# Fix for web-api preprocessing because it fails to handle missing trailing slashes
-# to_replace='.css">'
-# replacement='.css"\/>'
-# sed -i -e "s/$to_replace/$replacement/g" $file
-
-# Add detection of ressource loading errors
-# to_replace='<link rel="preload" '
-# replacement='<link rel="preload" onerror="on_load_error()" '
-# sed -i -e "s/$to_replace/$replacement/g" $file
-# to_replace='<link rel="modulepreload" '
-# replacement='<link rel="modulepreload" onerror="on_load_error()" '
-# sed -i -e "s/$to_replace/$replacement/g" $file
-
-
 replace() {
     sed -i "s|$1|$2|" "$TRUNK_STAGING_DIR/index.html"
 }
@@ -36,6 +10,15 @@ comment_out() {
 
 js_file=$(ls $TRUNK_STAGING_DIR | grep 'web-app-[a-zA-Z0-9]*\.js$')
 wasm_file=$(ls $TRUNK_STAGING_DIR | grep 'web-app-[a-zA-Z0-9]*_bg\.wasm$')
+
+# Juliette fixes
+to_replace="cachedBigInt64Memory0 = new BigInt64Array();"
+replacement="cachedBigInt64Memory0 = undefined; try { cachedBigInt64Memory0 = new BigInt64Array(); } catch (e) { console.error('BigInt64Array is not supported'); }"
+sed -i -e "s/$to_replace/$replacement/g" $js_file
+to_replace="cachedBigInt64Memory0 = new BigInt64Array(wasm.memory.buffer);"
+replacement="cachedBigInt64Memory0 = undefined; try { cachedBigInt64Memory0 = new BigInt64Array(wasm.memory.buffer); } catch (e) { console.error('BigInt64Array is not supported'); }"
+sed -i -e "s/$to_replace/$replacement/g" $js_file
+
 replace "JS_FILE" $js_file
 replace "WASM_FILE" $wasm_file
 replace "<script type=\"module\">" "<script>"
