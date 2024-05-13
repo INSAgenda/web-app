7eb65b9f6db0f49a0f59918cce8f85646d25e045
Dimitri <dimitri.timoz@orange.fr>
2024-05-13 13:43:00 +0200

    Fix post build for macos

--- post-build.sh
+++ post-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 replace() {
-    sed -i "s|$1|$2|" "$TRUNK_STAGING_DIR/index.html"
+    sed -i "" "s|$1|$2|" "$TRUNK_STAGING_DIR/index.html"
 }
 
 comment_out() {
@@ -14,10 +14,10 @@ wasm_file=$(ls $TRUNK_STAGING_DIR | grep 'web-app-[a-zA-Z0-9]*_bg\.wasm$')
 # Juliette fixes
 to_replace="cachedBigInt64Memory0 = new BigInt64Array();"
 replacement="cachedBigInt64Memory0 = undefined; try { cachedBigInt64Memory0 = new BigInt64Array(); } catch (e) { console.error('BigInt64Array is not supported'); }"
-sed -i -e "s/$to_replace/$replacement/g" $js_file
+sed -i "" -e "s/$to_replace/$replacement/g" $js_file
 to_replace="cachedBigInt64Memory0 = new BigInt64Array(wasm.memory.buffer);"
 replacement="cachedBigInt64Memory0 = undefined; try { cachedBigInt64Memory0 = new BigInt64Array(wasm.memory.buffer); } catch (e) { console.error('BigInt64Array is not supported'); }"
-sed -i -e "s/$to_replace/$replacement/g" $js_file
+sed -i "" -e "s/$to_replace/$replacement/g" $js_file
 
 replace "JS_FILE" $js_file
 replace "WASM_FILE" $wasm_file
