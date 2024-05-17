c9c213034f735f2deca25a51a6b27d57ae4591ba
Mubelotix <mubelotix@gmail.com>
2024-05-17 10:38:46 +0200

    Revert "Only send once"
    This reverts commit fee5f5f6a3f9e821ec32569b88e34e6ca5a82318.

--- src/pixelwar/pixelwar.rs
+++ src/pixelwar/pixelwar.rs
@@ -21,13 +21,102 @@ pub fn init_pixelwar(page: &Page, app_link: AppLink) -> web_sys::Element {
             if skip {
                 return;
             }
+        };
 
-            if e.origin() != PIXELWAR_IFRAME_URL {
-                log!("Received message from unknown origin {e:?}");
+        match ty.as_str() {
+            "cookies" => {
+                send_insaplace_message("getSatus", &JsValue::null());
+
+                let data = match data.dyn_into::<Array>() {
+                    Ok(data) => data.to_vec(),
+                    Err(_) => {
+                        log!("Received message from insaplace with invalid cookies data");
+                        return;
+                    }
+                };
+                let user_id = match data.first().and_then(|v| v.as_string()) {
+                    Some(user_id) => user_id,
+                    None => {
+                        log!("Received message from insaplace with invalid user_id");
+                        return;
+                    }
+                };
+                let user_token = match data.get(1).and_then(|v| v.as_string()) {
+                    Some(user_token) => user_token,
+                    None => {
+                        log!("Received message from insaplace with invalid user_token");
+                        return;
+                    }
+                };
+                let validation_token = match data.get(2).and_then(|v| v.as_string()) {
+                    Some(validation_token) => validation_token,
+                    None => {
+                        log!("Received message from insaplace with invalid validation_token");
+                        return;
+                    }
+                };
+                let member_id = match data.get(3).and_then(|v| v.as_string()) {
+                    Some(member_id) => member_id,
+                    None => {
+                        log!("Received message from insaplace with invalid member_id");
+                        return;
+                    }
+                };
+
+                let send_insaplace_message = send_insaplace_message.clone();
+                spawn_local(async move {
+                    let url = format!("set-insaplace-cookies?user_id={user_id}&user_token={user_token}&validation_token={validation_token}&member_id={member_id}");
+                    match api_get::<()>(url).await {
+                        Ok(_) => log!("Successfully set insaplace cookies"),
+                        Err(e) => {
+                            log!("Failed to set insaplace cookies: {e}");
+                        }
+                    };
+                    match api_get::<Vec<(UserDesc, InsaplaceCookies)>>("get-insaplace-cookies").await {
+                        Ok(mut r) => {
+                            log!("Successfully got insaplace friend cookies");
+                            
+                            r.insert(0, (UserDesc::new(0, String::from("Vous")), InsaplaceCookies {
+                                user_id: user_id.clone(),
+                                user_token: user_token.clone(),
+                                validation_token: validation_token.clone(),
+                                member_id: member_id.to_string(),
+                            }));
+                            let usernames = Array::new();
+                            let cookies = Array::new();
+                            for (user, user_cookies) in r {
+                                usernames.push(&JsValue::from_str(&user.get_username()));
+                                let array = Array::new();
+                                array.push(&JsValue::from_str(&user_cookies.user_id));
+                                array.push(&JsValue::from_str(&user_cookies.user_token));
+                                array.push(&JsValue::from_str(&user_cookies.validation_token));
+                                array.push(&JsValue::from_str(&user_cookies.member_id));
+                                cookies.push(&JsValue::from(array));
+                            }
+                            let data = js_sys::Object::new();
+                            Reflect::set(&data, &JsValue::from_str("usernames"), &JsValue::from(usernames)).unwrap();
+                            Reflect::set(&data, &JsValue::from_str("cookies"), &JsValue::from(cookies)).unwrap();
+                            send_insaplace_message("cookies", &JsValue::from(data));
+                            log!("Successfully sent insaplace friend cookies")
+                        }
+                        Err(e) => log!("Failed to get insaplace cookies: {e}"),
+                    };                
+                });
             }
-        }) as Box<dyn FnMut(_)>);
-        window().add_event_listener_with_callback("message", on_message.as_ref().unchecked_ref()).unwrap();
-        on_message.forget();
-    }
+            "canPlace" => {
+                if let Some(data) = data.as_bool()  {
+                    app_link.send_message(AppMsg::SetPixelLockedState(data));
+                } else {
+                    log!("Received message from insaplace with invalid canPlace data");
+                }
+            }
+            ty => {
+                log!("Received message from insaplace with unknown type {ty}");
+            }
+        }
+    }) as Box<dyn FnMut(_)>);
+    window().add_event_listener_with_callback("message", on_message.as_ref().unchecked_ref()).unwrap();
+    on_message.forget();
+    
     iframe
 }
